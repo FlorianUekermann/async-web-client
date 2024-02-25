85718cd331c0dcb0d4493668b5e5db8d90471d08
Frangen, Leon <lesoftware@proton.me>
2024-02-25 10:11:12 +0100

    add trait to disambigue IntoBodyRequest-Implementables from ()

--- src/http/body.rs
+++ src/http/body.rs
@@ -7,6 +7,13 @@ pub trait IntoRequestBody {
     fn into_request_body(self) -> (Self::RequestBody, u64);
 }
 
+pub trait NotEmptyBody: IntoRequestBody {}
+
+impl<'a, T: AsRef<[u8]>> NotEmptyBody for &'a T {}
+impl NotEmptyBody for Vec<u8> {}
+impl NotEmptyBody for String {}
+impl<T: NotEmptyBody> NotEmptyBody for Option<T> {}
+
 impl<'a, T: AsRef<[u8]>> IntoRequestBody for &'a T {
     type RequestBody = &'a [u8];
     fn into_request_body(self) -> (Self::RequestBody, u64) {
